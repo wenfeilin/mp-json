3b6950fa97161eaecf197b2b580720434f56577d
yezi <yezi@grinnell.edu>
2023-11-30 21:28:14 -0600

    Updated JSONConstant

--- JSONConstant.java
+++ JSONConstant.java
@@ -40,7 +40,7 @@ public class JSONConstant implements JSONValue {
   /**
    * Build a new constant.
    */
-  private JSONConstant(Object value) {
+  public JSONConstant(Object value) {
     this.value = value;
   } // JSONConstant
 
